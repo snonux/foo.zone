246d0f921bbc3e87103f59fc8b7f8d2cb2bc9f76
Paul Buetow <paul@buetow.org>
2023-03-15 00:59:30 +0200

    Update content for html

---
+++ template.gmi.tpl.sh
@@ -0,0 +1,17 @@
+cat <<END
+# This is a template
+
+> Hello world!
+
+Prices higher in Alaska and Hawaii.
+
+## Heading
+
+Love your enemies: they'll go crazy trying to figure out what you're up to.
+
+* Foo
+* Bar
+* Baz
+
+END
+cat ./notes/the-pragmatic-programmer.gmi
